27777fc3b8cf3f62ac3758ad99ac5fce140bfc31
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-09-19 20:22:55 +0100

    client : writeData , convert readed buffer to char vector , append it to req vector

--- server/Client.cpp
+++ server/Client.cpp
@@ -1,6 +1,6 @@
 #include "Client.hpp"
 
-Client::Client() {
+Client::Client(int fd):client_fd(fd), connected(true) {
     // Constructor implementation
 }
 
@@ -18,4 +18,42 @@ Client& Client::operator=(const Client& other) {
         // Perform deep copy
     }
     return *this;
+}
+
+int Client::getFd() const {
+    return client_fd;
+}
+
+bool Client::isConnected() const {
+    return connected;
+}
+
+bool Client::writeData(){
+    char buffer[BUFFER];
+
+    ssize_t bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+    if (bytes > 0)
+    {
+        /*
+            std::vector<int> AB = A;
+            AB.insert(AB.end(), B.begin(), B.end());
+        */
+       std::vector<char> readed(buffer, buffer + bytes);
+       read_buffer.insert(read_buffer.end(), readed.begin(), readed.end());
+       
+        return true;
+    }
+    else if (bytes == 0)
+    {
+        connected = false; // Client disconnected
+    }
+    // else if (errno != EAGAIN && errno != EWOULDBLOCK)
+    // {
+    //     connected = false; // Error occurred
+    // }
+    return false;
+}
+
+bool Client::readData(){
+
 }
