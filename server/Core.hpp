391b997317d8b777e13112eccc5075e6d7aa5171
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-09-20 14:40:09 +0100

    algo core

--- server/Core.hpp
+++ server/Core.hpp
@@ -1,24 +1,34 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 #include"Socket.hpp"
+#include"Client.hpp"
 #include"EventLoop.hpp"
 #include<vector>
 
 class Core {
-    private :
+    private:
         std::vector<Socket*> servers;
-        // std::map<int, Client*> clients;
-        EventLoop loop ;
-        bool riunning;
-    public :
-        Core();
-        ~Core();
-        Core(const Core&);
-        Core& operator=(const Core&);
+        std::map<int, Client*> clients;
+        EventLoop event_loop;
+        bool running;
+        // Config obj;
 
-    private :
-        
+        void handleSocketEvent(int fd, short events);
+        bool isServerSocket(int fd);
+        void handleNewConnection(int server_fd);
+        void handleClientEvent(int client_fd, short events);
+        void processClientRequest(Client *client);
 
+
+
+    public:
+        MultiServer();
+        ~MultiServer();
+        MultiServer(const MultiServer& other);
+        MultiServer& operator=(const MultiServer& other);
+        bool addServer(int port);
+        void run();
+        void stop();
 };
 
 #endif
