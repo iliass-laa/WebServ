69efad88bd426a58a5c36bb9f550a6d2994756ba
Fouad Idriss <fidriss@c1r6p1.1337.ma>
2025-09-21 16:16:08 +0100

    Completed header parsing + checks if request was received in full

--- req-res/request.cpp
+++ req-res/request.cpp
@@ -1,11 +1,21 @@
 #include "request.hpp"
 
-
-struct HttpRequest parseRequest(std::vector<char> requestBuffer){
+int handleRequest(std::vector<char> requestBuffer){
     struct HttpRequest Req;
+    
+    if (parseRequest(requestBuffer, Req) == INCOMPLETE)
+        return INCOMPLETE;
+    else if (parseRequest(requestBuffer, Req) == ERROR)
+        return ERROR;
+    return 0;
+}
+
+int parseRequest(std::vector<char> requestBuffer, struct HttpRequest &Req) {
 
     std::string requestString(requestBuffer.begin(), requestBuffer.end());
     size_t pos = requestString.find("\r\n\r\n");
+    if (pos == std::string::npos)
+        return INCOMPLETE;
     std::string headerPart = requestString.substr(0, pos);
     std::string bodyPart = requestString.substr(pos + 4);
     std::istringstream headerStream(headerPart);
@@ -14,8 +24,33 @@ struct HttpRequest parseRequest(std::vector<char> requestBuffer){
     std::istringstream reqLineStream(reqLine);
 
     reqLineStream >> Req.method >> Req.uri >> Req.version;
+    if (Req.method.empty() || Req.uri.empty() || Req.version.empty())
+        return ERROR;
     std::string headerLine;
-    while (std::getline(headerStream, headerLine) && headerLine != "\r") {
-        //parse header
+    while (std::getline(headerStream, headerLine)) {
+        if (headerLine == "\r")
+            break;
+        size_t colonPos = headerLine.find(":");
+        if (colonPos == std::string::npos)
+            return ERROR;
+        std::string key = headerLine.substr(0, colonPos);
+        std::string value = headerLine.substr(colonPos + 1);
+        if (!key.empty() && key[0] == ' ')
+            key.erase(0, 1);
+        if (!value.empty() && value[value.size()- 1] == '\r')
+            value.erase(value.size() - 1);
+        Req.headers[key] = value;
     }
+    if (Req.headers.find("Content-Length") != Req.headers.end()) {
+        int contentLength = atoi(Req.headers["Content-Length"].c_str());
+        if ((int)bodyPart.size() < contentLength)
+            return INCOMPLETE;
+    }
+    else if (Req.headers.find("Transfer-Encoding") != Req.headers.end() &&
+             Req.headers["Transfer-Encoding"] == "chunked") {
+        if (!isChunkedBodyComplete(bodyPart)) //Chunked body parse
+            return INCOMPLETE;
+    }
+    Req.body = bodyPart;
+    return COMPLETE;
 }
