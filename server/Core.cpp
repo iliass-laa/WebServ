00c49d41fb1895350f10f6a8d495f801979b8c2c
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-10-11 00:42:46 +0100

    server : fixed send in chunks , we need to test getting vedio or image

--- server/Core.cpp
+++ server/Core.cpp
@@ -110,6 +110,9 @@ void Core::handleClientEvent(int client_fd, short events){
         // std::cout << " **** Sending Response ****"<< std::endl;
         if(client->getClientState() == WAITTING_FOR_RESPONSE || client->getClientState() == SENDING_RESPONSE )
             client->writeData();
+        if(client->getClientState() == WAITTING_FOR_REQUEST)
+            event_loop.updateSocketEvents(client->getFd() ,POLLIN );
+
     }
     
     // disconnect the client 
